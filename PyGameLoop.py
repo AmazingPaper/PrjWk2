b8e94b5117b9f7c129ccb45e85d4a538fb5e724c
Mark Slingerlad <markslingerland@hotmail.com>
2016-01-19 12:53:18 +0100

    Added colors and fixed the Clock bug

--- PyGameLoop.py
+++ PyGameLoop.py
@@ -1,14 +1,39 @@
 import pygame
+import sys
+import os
 
 pygame.init()
 
-setHeight = 600                                                 # Set resolution
-setWidth = 800                                                  # Also resolution
+FPS = 60 # frames per second, the general speed of the program
+WINDOWWIDTH = 800 # size of window's width in pixels
+WINDOWHEIGHT = 600 # size of windows' height in pixels
+REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+BOXSIZE = 40 # size of box height & width in pixels
+GAPSIZE = 10 # size of gap between boxes in pixels
+BOARDWIDTH = 10 # number of columns of icons
+BOARDHEIGHT = 8 # number of rows of icons
+assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
 
-gameDisplay = pygame.display.set_mode((setHeight, setWidth))    # Apply resolution to game window
+gameDisplay = pygame.display.set_mode((WINDOWHEIGHT, WINDOWWIDTH))    # Apply resolution to game window
 pygame.display.set_caption('Survivor')                          # Title of project
+#----------------------------------------------------------------------------------------------------------------------
+#Colorlist
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+#----------------------------------------------------------------------------------------------------------------------
 
-clock = pygame.Clock()                                          # Add clock
+clock = pygame.time.Clock()                                         # Add clock
 
 crashed = False                                                 # Crash handler
 
