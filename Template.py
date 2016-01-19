f3f24c62ff4da30634267ecac12e36e6f9984044
Damien van Offeren <0823762@hr.nl>
2016-01-19 15:14:14 +0100

    Brushed up 'Template'

--- Template.py
+++ Template.py
@@ -1,29 +1,3 @@
-
-# #This files contains little snippets of code that can be repurposed
-# list = []
-#
-# def PlayerCount():
-#         x = int(input("Please specify how many players will enter the game: "))
-#         for i in range(0, x):
-#             name = str(input("Please enter your name: "))
-#             list.append([name])
-#             list.append([i + 1])
-#         if x == 2:
-#                 print("Player " + str(list[0]).strip("[']") + " has player number " + str(list[1]).strip("[]") + " and is ready to go!")
-#                 print("Player " + str(list[2]).strip("[']") + " has player number " + str(list[3]).strip("[]") + " and is ready to go!")
-#
-#         elif x == 3:
-#                 print("Player " + str(list[4]).strip("[']") + " has player number " + str(list[5]).strip("[]") + " and is ready to go!")
-#
-#         else:
-#                 print("Player " + str(list[6]).strip("[']") + " has player number " + str(list[7]).strip("[]") + " and is ready to go!")
-#
-#
-# PlayerCount()
-
-from cl import *
-
-
 list = []
 
 def PlayerCount():
@@ -33,11 +7,4 @@ def PlayerCount():
         list.append([i + 1])
 
 PlayerCount()
-
-
-
-# print("Player " + str(list[6]).strip("[']") + " has player number " + str(list[7]).strip("[]") + " and is ready to go!")
-
-
-
 
