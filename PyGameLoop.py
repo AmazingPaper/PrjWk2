8a6b71a6a4e36b602d057d0d91bb1b312f146c2e
Mark Slingerlad <markslingerland@hotmail.com>
2016-01-19 13:47:27 +0100

    Added a colorlist and a background for the boardgame

--- PyGameLoop.py
+++ PyGameLoop.py
@@ -1,47 +1,29 @@
-import pygame
-import sys
+import pygame, sys
 import os
+import cl
+
+from pygame.locals import *
 
 pygame.init()
 
-FPS = 60 # frames per second, the general speed of the program
-WINDOWWIDTH = 800 # size of window's width in pixels
-WINDOWHEIGHT = 600 # size of windows' height in pixels
-REVEALSPEED = 8 # speed boxes' sliding reveals and covers
-BOXSIZE = 40 # size of box height & width in pixels
-GAPSIZE = 10 # size of gap between boxes in pixels
-BOARDWIDTH = 10 # number of columns of icons
-BOARDHEIGHT = 8 # number of rows of icons
-assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
-XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
-YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
-
-gameDisplay = pygame.display.set_mode((WINDOWHEIGHT, WINDOWWIDTH))    # Apply resolution to game window
+DISPLAYSURF = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Survivor')                          # Title of project
-#----------------------------------------------------------------------------------------------------------------------
-#Colorlist
-#            R    G    B
-GRAY     = (100, 100, 100)
-NAVYBLUE = ( 60,  60, 100)
-WHITE    = (255, 255, 255)
-RED      = (255,   0,   0)
-GREEN    = (  0, 255,   0)
-BLUE     = (  0,   0, 255)
-YELLOW   = (255, 255,   0)
-ORANGE   = (255, 128,   0)
-PURPLE   = (255,   0, 255)
-CYAN     = (  0, 255, 255)
-#----------------------------------------------------------------------------------------------------------------------
 
-clock = pygame.time.Clock()                                         # Add clock
 
+
+clock = pygame.time.Clock()                                     # Add clock
 crashed = False                                                 # Crash handler
+
+background = pygame.Surface(DISPLAYSURF.get_size()) # Gets the size of the screen
+background = background.convert()
+background.fill((cl.YELLOW))            # Fills the screen with the given color in the cl
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
+        DISPLAYSURF.blit(background, (0, 0))                    # Shows us the background
         pygame.display.update()                                 # Can also be changed to 'pygame.display.flip()'
         clock.tick(60)                                          # Set FPS, PC MASTER RACE
 
