114e4c5392fc3ebe7d082105a7c26af539a23cbb
0909023 <0909023@hr.nl>
2016-01-20 13:02:39 +0100

    GameMenu function, 8bit font, 2 images, Menu Music file
    update note:
    GameMenu alleen EXIT knop werkt, Fightknop kan als als de gameloop als
    functie is gedefineerd.
    
    in cl.py heb ik 4 kleuren toegevoegd. SURV_BLUE is de standaard survival
    blauw kleur.
    
    ik heb een leuke 8bit font gevonden. Roep deze aan door:
    pygame.font.Font('8-BIT WONDER.TTF',groote)
    
    2 plaatjes, het siluet van de boxer en een kleine box handschoen.
    roep deze aan door: "image\naamplaatje.png

--- GameMenu.py
+++ GameMenu.py
@@ -1,80 +1,123 @@
 import pygame
-import time
-
-pygame.init()
-
-display_width = 800
-display_height = 600
-
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-
-car_width = 73
-
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('A bit Racey')
-clock = pygame.time.Clock()
-
+from PyGameLoop import *
+from cl.py      import *
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+    textSurface = font.render(text, True, SURV_BLUE)
     return textSurface, textSurface.get_rect()
 
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf',115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
+def text_objects2 (text, font):
+    textSurface = font.render(text, True, WHITE)
+    return textSurface, textSurface.get_rect()
 
-    pygame.display.update()
+def text_objects3 (text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
 
-    time.sleep(2)
+#sounds
+def intro_music():
+    pygame.mixer.music.load("Sounds/Sandstorm.mp3")
+    pygame.mixer.music.play()
 
-    game_loop()
+def intro_menu():
+    intro = True
+    if intro == True:
+        intro_music()
 
-
-
-def crash():
-    message_display('You Crashed')
-
-def game_loop():
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
-
-    x_change = 0
-
-    gameExit = False
-
-    while not gameExit:
-
+    while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x_change = -5
-                if event.key == pygame.K_RIGHT:
-                    x_change = 5
+        #text objects
+        DISPLAYSURF.fill(SURV_BLUE)
+        pygame.draw.rect(DISPLAYSURF,YELLOW,(20,70,570,100)) #title box
+        largeText = pygame.font.Font('8-BIT WONDER.TTF',75)
+        TextSurf, TextRect = text_objects("SURVIVOR", largeText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/1.95),((MAPHEIGHT*TILESIZE)/5))
+        DISPLAYSURF.blit(TextSurf, TextRect)
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
+        smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+        TextSurf, TextRect = text_objects2("MENU", smallText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/2),((MAPHEIGHT*TILESIZE)/12))
+        DISPLAYSURF.blit(TextSurf, TextRect)
 
-        x += x_change
+        #images
+        img = pygame.image.load('images/survivorstockimage.png')
+        DISPLAYSURF.blit(img,(20,180))
 
-        gameDisplay.fill(white)
-        message_display("Menu of Survival")
+        img2 = pygame.image.load('images/boximg.png')
 
-        if x > display_width - car_width or x < 0:
-            crash()
+        #button objects
+        #play
+        pygame.draw.rect(DISPLAYSURF,DIM_RED,(250,400,100,50))
+        #quit
+        pygame.draw.rect(DISPLAYSURF,DIM_GREY,(50,500,100,50))
+        #settings
+        pygame.draw.rect(DISPLAYSURF,DIM_BLUE,(450,500,100,50))
+        #rules
+        pygame.draw.rect(DISPLAYSURF,DIM_YELLOW,(250,500,100,50))
 
+        #interactive button
+        #als muis tussen x vh object (250+100) en begin (250) zit en
+        #muis zit tussen y vh object (400+50) en begin vh object (400)
+        mouseLocation = pygame.mouse.get_pos()
+        mouseClick    = pygame.mouse.get_pressed()
+
+        if 250+100 > mouseLocation[0] > 250 and 400 + 50 > mouseLocation[1] > 400:
+            pygame.draw.rect(DISPLAYSURF,RED,(250,400,100,50))
+            DISPLAYSURF.blit(img2,(200,400))
+            if mouseClick[0] == 1:
+                None        #SHOULD run Gameloop function
+
+        else:
+            pygame.draw.rect(DISPLAYSURF,DIM_RED,(250,400,100,50))
+
+        if 50+100 > mouseLocation[0] > 50 and 500 + 50 > mouseLocation[1] > 500:
+            pygame.draw.rect(DISPLAYSURF,GREY,(50,500,100,50))
+            DISPLAYSURF.blit(img2,(0,500))
+            if mouseClick[0] == 1:
+                pygame.quit()
+                quit()
+        else:
+            pygame.draw.rect(DISPLAYSURF,DIM_GREY,(50,500,100,50))
+
+        if 450+100 > mouseLocation[0] > 450 and 500 + 50 > mouseLocation[1] > 500:
+            pygame.draw.rect(DISPLAYSURF,BLUE,(450,500,100,50))
+            DISPLAYSURF.blit(img2,(400,500))
+        else:
+            pygame.draw.rect(DISPLAYSURF, DIM_BLUE, (450,500,100,50))
+
+        if 250+100 > mouseLocation[0] > 250 and 500 + 50 > mouseLocation[1] > 500:
+            pygame.draw.rect(DISPLAYSURF,YELLOW,(250,500,100,50))
+            DISPLAYSURF.blit(img2,(200,500))
+        else:
+            pygame.draw.rect(DISPLAYSURF,DIM_YELLOW,(250,500,100,50))
+
+        #text_of_buttons (250,400,100,50))
+
+        smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+        TextSurf, TextRect = text_objects3("EXIT", smallText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/6),((MAPHEIGHT*TILESIZE)/1.15))
+        DISPLAYSURF.blit(TextSurf, TextRect)
+
+        smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+        TextSurf, TextRect = text_objects3("FIGHT", smallText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/2.01),((MAPHEIGHT*TILESIZE)/1.43))
+        DISPLAYSURF.blit(TextSurf, TextRect)
+
+        smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+        TextSurf, TextRect = text_objects3("OPTIONS", smallText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/1.21),((MAPHEIGHT*TILESIZE)/1.15))
+        DISPLAYSURF.blit(TextSurf, TextRect)
+
+        smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+        TextSurf, TextRect = text_objects3("RULES", smallText)
+        TextRect.center = (((MAPWIDTH*TILESIZE)/2),((MAPHEIGHT*TILESIZE)/1.15))
+        DISPLAYSURF.blit(TextSurf, TextRect)
 
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(30)
 
-game_loop()
-pygame.quit()
-quit()
+intro_menu()
