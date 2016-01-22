2ba9fd97605f56e2b052a4ba65d8220ff333215f
0915144 <0915144@hr.nl>
2016-01-22 09:38:10 +0100

    added player class

--- Fighter Classes.py
+++ Fighter Classes.py
@@ -11,3 +11,11 @@ class Char:
 
     def __str__(self):
         return "This is {},\nhis damage with 1 = {},\nhis damage with 2 = {},\nhis damage with 3 = {},\nhis damage with 4 = {},\nhis damage with 5 = {},\nand his damage with 6 = {}".format(self.name, self.n1, self.n2,self.n3, self.n4, self.n5, self.n6)
+
+class Player123:
+    def __init__(self, name, LP, CP, posx, posy):
+        self.Name = name
+        self.Lifepoints = LP
+        self.Conditionpoints = CP
+        self.positionx = posx
+        self.positiony = posy
