f46357d2e9f873fd22c6b9858518723228206e93
Elif Celik <elifyildizcelik@gmail.com>
2016-01-29 11:39:28 +0100

    Imported introscene

---
+++ Scenes/SceneBase.py
@@ -0,0 +1,32 @@
+import random
+
+import pygame
+
+
+class SceneBase:
+	def __init__(self):
+		self.next = self
+
+	def ProcessInput(self, events, pressed_keys):
+		print("uh-oh, you didn't override this in the child class")
+
+	def Update(self):
+		print("uh-oh, you didn't override this in the child class")
+
+	def Render(self, screen):
+		print("uh-oh, you didn't override this in the child class")
+
+	def SwitchToScene(self, next_scene):
+		self.next = next_scene
+
+	def Terminate(self):
+		self.SwitchToScene(None)
+
+	def quitGame(self):
+		pygame.quit()
+		quit()
+
+	# Ability to roll die.
+	def dieRoll(self):
+		number = random.randint(1, 6)
+		return number
