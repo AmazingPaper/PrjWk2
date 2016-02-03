e7f4657167b597a27f98bafb39cb07d0b8e81c90
0909023 <0909023@hr.nl>
2016-02-03 13:04:07 +0100

    Boxgloves are white where they should be

--- Scenes/RulesScene.py
+++ Scenes/RulesScene.py
@@ -21,10 +21,28 @@ class RulesScene(SceneBase):
 
 		screen.fill(SURV_BLUE)
 
-		smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
-		TextSurf, TextRect = text_objects2("Rules", smallText)
+		pygame.draw.rect(screen, YELLOW, (245, 0, 115, 70))
+
+		smallText = pygame.font.Font('8-BIT WONDER.TTF', 20)
+		TextSurf, TextRect = text_objects("Rules", smallText)
 		TextRect.center = (((MapWidth * TileSize) / 2), ((MapHeight * TileSize) / 12))
 		screen.blit(TextSurf, TextRect)
+
+		smallText = pygame.font.Font('8-BIT WONDER.TTF', 12)
+		TextSurf, TextRect = text_objects2("GAME PAUSED", smallText)
+		TextRect.center = ((100), ((MapHeight * TileSize) / 20))
+		screen.blit(TextSurf, TextRect)
+
+		smallText = pygame.font.Font('8-BIT WONDER.TTF', 12)
+		TextSurf, TextRect = text_objects2("GAME PAUSED", smallText)
+		TextRect.center = ((500), ((MapHeight * TileSize) / 20))
+		screen.blit(TextSurf, TextRect)
+
+		smallText = pygame.font.Font('8-BIT WONDER.TTF', 15)
+		TextSurf, TextRect = text_objects2("PRESS ANY BUTTON TO GO BACK", smallText)
+		TextRect.center = (((MapWidth * TileSize) / 2), (760))
+		screen.blit(TextSurf, TextRect)
+
 
 		screen.blit(self.game.images['rules'], (40, 80))
 
